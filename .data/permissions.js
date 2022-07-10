00347ceeb6a91a19cde9eb65b8006f8b5ccfdb66
Richard Lovell <rjlovell@hotmail.com>
2022-07-10 20:56:12 +1200

    First commit

---
+++ .data/permissions.js
@@ -0,0 +1,2 @@
+const permissions = {"primary-menu-item":["find","findOne"],"primary-menu-items":["find","findOne"],"social-link":["find","findOne"],"social-links":["find","findOne"],"global":["find","findOne"],"layout":["find","findOne"],"home":["find","findOne"],"contact":["find","findOne"],"service":["find","findOne"]};
+  module.exports = permissions;
